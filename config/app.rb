01d8f11462a098cd8b1ed156c8013585128380fd
Aotokitsuruya <contact@aotoki.me>
2024-05-29 20:45:39 +0800

    Add hanami-lambda

--- config/app.rb
+++ config/app.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 require 'hanami'
+require 'hanami/lambda'
 
 module SmartAssist
   class App < Hanami::App
+    extend Hanami::Lambda::Application
   end
 end
---
+++ config/lambda.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module SmartAssist
+  class Lambda < Hanami::Lambda::Dispatcher
+  end
+end
