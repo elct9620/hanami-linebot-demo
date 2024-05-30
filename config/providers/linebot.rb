b62dfb9eed83f10d43ff71528b6afb4a8eab2bc3
Aotokitsuruya <contact@aotoki.me>
2024-05-30 20:23:24 +0800

    Register line bot client

---
+++ config/providers/linebot.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+Hanami.app.register_provider(:line_bot) do
+  prepare do
+    require 'line/bot'
+  end
+
+  start do
+    register('line_bot', Line::Bot::Client.new do |config|
+                           config.channel_id = target[:settings].line_channel_id
+                           config.channel_secret = target[:settings].line_channel_secret
+                           config.channel_token = target[:settings].line_channel_token
+                         end)
+  end
+end
--- config/settings.rb
+++ config/settings.rb
@@ -5,5 +5,8 @@ module SmartAssist
     # Define your app settings here, for example:
     #
     # setting :my_flag, default: false, constructor: Types::Params::Bool
+    setting :line_channel_id, constructor: Types::Params::String
+    setting :line_channel_token, constructor: Types::Params::String
+    setting :line_channel_secret, constructor: Types::Params::String
   end
 end
