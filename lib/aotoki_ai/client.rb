1b9f41e280973a59851be83cda62c8aaf113309e
Aotokitsuruya <contact@aotoki.me>
2024-10-19 21:48:04 +0800

    Fix endpoint and API path

--- lib/aotoki_ai/client.rb
+++ lib/aotoki_ai/client.rb
@@ -6,10 +6,10 @@ module AotokiAi
   require_relative 'suggest'
 
   class Client < Async::REST::Resource
-    ENDPOINT = Async::HTTP::Endpoint.parse('https://blog.aotoki.me/ai')
+    ENDPOINT = Async::HTTP::Endpoint.parse('https://blog.aotoki.me')
 
     def suggest(message, &)
-      Suggest.post(with(path: '/v1/suggest'), message:, &)
+      Suggest.post(with(path: '/ai/v1/suggest'), message:, &)
     end
   end
 end
